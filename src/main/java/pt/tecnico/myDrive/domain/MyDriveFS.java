fd131f191c2f175e3b4849e3e31317ddafa9dcb5
bernardocaetano <bernas999@hotmail.com>
2016-03-08 18:29:42 +0000

    Add MyDriveFS Constructor and redefine getInstance

--- src/main/java/pt/tecnico/myDrive/domain/MyDriveFS.java
+++ src/main/java/pt/tecnico/myDrive/domain/MyDriveFS.java
@@ -1,9 +1,23 @@
 package pt.tecnico.myDrive.domain;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.ist.fenixframework.FenixFramework;
+
 public class MyDriveFS extends MyDriveFS_Base {
     
+	public static MyDriveFS getInstance() {
+        MyDriveFS fs = FenixFramework.getDomainRoot().getMyDriveFS();
+        if (fs != null)
+	    return fs;
+
+		log.trace("new MyDriveFS");
+        return new MyDriveFS();
+    }
+
     public MyDriveFS() {
-        super();
+        setRoot(FenixFramework.getDomainRoot());
     }
     
 }
