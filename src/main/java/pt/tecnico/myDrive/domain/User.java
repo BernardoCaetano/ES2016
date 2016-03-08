bdba61cbf583fa384d1b03ce1586b19551994d5c
bernardocaetano <bernas999@hotmail.com>
2016-03-08 16:56:10 +0000

    Add User Constructor

--- src/main/java/pt/tecnico/myDrive/domain/User.java
+++ src/main/java/pt/tecnico/myDrive/domain/User.java
@@ -1,9 +1,18 @@
 package pt.tecnico.myDrive.domain;
 
 public class User extends User_Base {
-    
-    public User() {
-        super();
+            
+    public User(String username) {
+    	setUsername(username);
+    	setPassword(username);
+    	setName(username);
+    	setUmask("rwxd----");        
     }
-    
+
+    public User(String username, String password, String name, String umask) {
+    	setUsername(username);
+    	setPassword(username);
+    	setName(username);
+    	setUmask(umask);
+    }
 }
