9dc7d6d8609ea32758ec1acc48343dd0fa0e8aa3
Luís Silva <luis.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-03-09 11:12:56 +0000

    Add Directory.getFileByName() method

--- src/main/java/pt/tecnico/myDrive/domain/Directory.java
+++ src/main/java/pt/tecnico/myDrive/domain/Directory.java
@@ -6,4 +6,19 @@ public class Directory extends Directory_Base {
         super();
     }
     
+    
+    public AbstractFile getChild(String name){
+    	if( name.equals(".") ){
+    		return this;
+    	}else if( name.equals("..") ){
+    		return this.getParent();
+    	}
+    	for( AbstractFile f : getFilesSet() ){
+    		if( f.getName().equals(name) ){
+    			return f;
+    		}
+    	}
+    	return null;
+    }
+    
 }
