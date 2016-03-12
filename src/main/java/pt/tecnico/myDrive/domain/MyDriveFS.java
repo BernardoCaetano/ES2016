8647232e1cad33492b58427bacc2ada63468cf60
Catarina Cepeda <catarinacepeda@gmail.com>
2016-03-12 03:08:43 +0000

    Add MyDriveFS.listDirectorySorted() and Directory.getFilesSimpleSorted() methods
    Note: the functionality related to these two methods has not been tested due to a bug in some of the constructors.

--- src/main/java/pt/tecnico/myDrive/domain/MyDriveFS.java
+++ src/main/java/pt/tecnico/myDrive/domain/MyDriveFS.java
@@ -1,57 +1,59 @@
 package pt.tecnico.myDrive.domain;
 
+import java.util.ArrayList;
+
 import pt.ist.fenixframework.FenixFramework;
 import pt.tecnico.myDrive.exception.FileNotFoundException;
 import pt.tecnico.myDrive.exception.InvalidPathException;
 import pt.tecnico.myDrive.exception.NameAlreadyExistsException;
 
 public class MyDriveFS extends MyDriveFS_Base {
-    
+
 	public static MyDriveFS getInstance() {
-        MyDriveFS mydrive = FenixFramework.getDomainRoot().getMyDrive();
-        if (mydrive != null)
-	    return mydrive;
+		MyDriveFS mydrive = FenixFramework.getDomainRoot().getMyDrive();
+		if (mydrive != null)
+			return mydrive;
 
-        MyDriveFS newMyDrive = new MyDriveFS();
-        RootDirectory rootDir = RootDirectory.getInstance(newMyDrive);
-        newMyDrive.setRootDirectory(rootDir);
-        User root = new SuperUser(newMyDrive, "root", "***", "Super User", "rwxdr-x-");
-        rootDir.setOwner(root);
-        return newMyDrive;
-    }
+		MyDriveFS newMyDrive = new MyDriveFS();
+		RootDirectory rootDir = RootDirectory.getInstance(newMyDrive);
+		newMyDrive.setRootDirectory(rootDir);
+		User root = new SuperUser(newMyDrive, "root", "***", "Super User", "rwxdr-x-");
+		rootDir.setOwner(root);
+		return newMyDrive;
+	}
 
-    private MyDriveFS() {
-        setRoot(FenixFramework.getDomainRoot());
-    }
+	private MyDriveFS() {
+		setRoot(FenixFramework.getDomainRoot());
+	}
 
-    @Override
-    public void addUsers(User userToBeAdded) throws NameAlreadyExistsException {
-        if (hasUser(userToBeAdded.getUsername())){
-            throw new NameAlreadyExistsException(userToBeAdded.getUsername());
-        }
+	@Override
+	public void addUsers(User userToBeAdded) throws NameAlreadyExistsException {
+		if (hasUser(userToBeAdded.getUsername())) {
+			throw new NameAlreadyExistsException(userToBeAdded.getUsername());
+		}
 
-        super.addUsers(userToBeAdded);
-    }
+		super.addUsers(userToBeAdded);
+	}
 
-    public boolean hasUser(String username) {
-        return getUserByUsername(username) != null;
-    }
+	public boolean hasUser(String username) {
+		return getUserByUsername(username) != null;
+	}
 
-    public User getUserByUsername(String username) {
-        for (User user : getUsersSet()) {
-            if (user.getUsername().equals(username)) {
-                return user;
-            }
-        }
-        return null;
-    }
+	public User getUserByUsername(String username) {
+		for (User user : getUsersSet()) {
+			if (user.getUsername().equals(username)) {
+				return user;
+			}
+		}
+		return null;
+	}
 
-    public void incrementLastFileID() {
-        super.setLastFileID(getLastFileID() + 1);
-    }
-    
+	public void incrementLastFileID() {
+		super.setLastFileID(getLastFileID() + 1);
+	}
+
 	public AbstractFile getFileByPath(Directory currentDir, String path)
-			throws FileNotFoundException, InvalidPathException{
+			throws FileNotFoundException, InvalidPathException {
 		if (path.equals("/")) {
 			return getRootDirectory();
 		} else if (path.startsWith("/")) {
@@ -60,8 +62,8 @@ public class MyDriveFS extends MyDriveFS_Base {
 		}
 
 		String[] parts = path.split("/", 2);
-		
-		if( parts.length == 0){
+
+		if (parts.length == 0) {
 			throw new InvalidPathException(path);
 		}
 
@@ -70,14 +72,34 @@ public class MyDriveFS extends MyDriveFS_Base {
 		if (parts.length < 2) {
 			return f;
 		}
-		
+
 		try {
 			return getFileByPath((Directory) f, parts[1]);
-		} catch (InvalidPathException e){
+		} catch (InvalidPathException e) {
 			throw new InvalidPathException(path);
-		} catch (FileNotFoundException e){
+		} catch (FileNotFoundException e) {
 			throw new FileNotFoundException(path);
 		}
+	}
+
+	public ArrayList<String> listDirectorySorted(Directory currentDir, String path) throws InvalidPathException {
+
+		AbstractFile dir = getFileByPath(currentDir, path);
+		if (!(dir instanceof Directory)) {
+			throw new InvalidPathException(path);
+		}
+
+		ArrayList<AbstractFile> files = ((Directory) dir).getFilesSimpleSorted();
+		ArrayList<String> filenames = new ArrayList<String>();
+		filenames.add(".");
+		filenames.add("..");
+
+		for (AbstractFile f : files) {
+			filenames.add(f.getName());
+		}
+
+		return filenames;
+
 	}
 
 }
