9f58b76b2d8d0e8b4223e3167233e3087a976e94
bernardocaetano <bernas999@hotmail.com>
2016-03-13 21:01:39 +0000

    Add new UserNotFoundException

---
+++ src/main/java/pt/tecnico/myDrive/exception/UserNotFoundException.java
@@ -0,0 +1,19 @@
+package pt.tecnico.myDrive.exception;
+
+public class UserNotFoundException extends MyDriveException {
+
+    private static final long serialVersionUID = 1L;
+
+    private String _username;
+
+    public UserNotFoundException(String username) {
+        _username = username;
+    }
+
+    public String getUnknownPath() { return _username; }
+
+    @Override
+    public String getMessage() {
+        return "The user '" + _username + "' does not exist";
+    }
+}
