03801867c82d290c7d726df6fdb6325f4a0dcf89
marinaferreira9 <msf923@gmail.com>
2016-03-13 23:11:43 +0000

    Add new exception NotDirectoryException

---
+++ src/main/java/pt/tecnico/myDrive/exception/NotDirectoryException.java
@@ -0,0 +1,21 @@
+package pt.tecnico.myDrive.exception;
+
+public class NotDirectoryException extends MyDriveException {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String dirName;
+
+	public NotDirectoryException(String dirName) {
+        this.dirName = dirName;
+    }
+
+	public String getDirName() {
+		return dirName;
+	}
+
+	@Override
+	public String getMessage() {
+		return "File '" + dirName + "' is not a directory";
+	}
+}
