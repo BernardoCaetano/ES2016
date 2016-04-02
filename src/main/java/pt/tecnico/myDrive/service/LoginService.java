9b2a9c1fcd57162f32914c8c47ed372497f2376b
Luís Silva <luis.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-04-02 03:08:23 +0100

    Add LoginService base
    Does nothing yet. Needed for testing (#17).

---
+++ src/main/java/pt/tecnico/myDrive/service/LoginService.java
@@ -0,0 +1,21 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class LoginService extends MyDriveService {
+
+	public LoginService(String username, String password) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void dispatch() throws MyDriveException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public final long result() {
+		// TODO
+		return 0; // FIXME
+	}
+}
