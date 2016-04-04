2e63a417a568346a0fc100fa8475606e31354c5d
marinaferreira9 <msf923@gmail.com>
2016-04-05 00:27:30 +0100

    Add DeleteFile service base

---
+++ src/main/java/pt/tecnico/myDrive/service/DeleteFileService.java
@@ -0,0 +1,18 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class DeleteFileService extends MyDriveService {
+
+	public DeleteFileService(long token, String name) {
+		// TODO 
+	}
+
+	@Override
+	protected void dispatch() throws MyDriveException {
+		// TODO
+
+	}
+
+	
+}
