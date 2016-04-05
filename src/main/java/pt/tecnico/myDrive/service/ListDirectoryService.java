849576522cb9053cab438e80d9a3771b8a8445f1
Daniel Madruga <madrugadjn@gmail.com>
2016-04-05 22:51:13 +0100

    Add ListDirectoryService file

---
+++ src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
@@ -0,0 +1,18 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class ListDirectoryService extends MyDriveService {
+
+	public ListDirectoryService(long token) {
+		// TODO 
+	}
+
+	@Override
+	protected void dispatch() throws MyDriveException {
+		// TODO
+
+	}
+
+	
+}
