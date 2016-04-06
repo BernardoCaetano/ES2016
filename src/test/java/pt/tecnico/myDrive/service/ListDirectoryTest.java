55178de2c83c6534a723f2a631f8e21656af36f3
Daniel Madruga <madrugadjn@gmail.com>
2016-04-06 02:21:11 +0100

    Add ListDirectoryTest

---
+++ src/test/java/pt/tecnico/myDrive/service/ListDirectoryTest.java
@@ -0,0 +1,18 @@
+package pt.tecnico.myDrive.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import pt.tecnico.myDrive.domain.*;
+
+public class ListDirectoryTest extends TokenReceivingTest {
+	
+	@Override
+	public void populate() {
+	}
+	
+	
+}
