9d4b51df1312f85f9ea9487e7c83b47311b07ea2
marinaferreira9 <msf923@gmail.com>
2016-04-06 01:27:43 +0100

    Fix delete file tests
    Creation of directories and files changed from populate() method to their respective tests

--- src/test/java/pt/tecnico/myDrive/service/DeleteFileTest.java
+++ src/test/java/pt/tecnico/myDrive/service/DeleteFileTest.java
@@ -1,6 +1,5 @@
 package pt.tecnico.myDrive.service;
 
-
 import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
@@ -17,62 +16,28 @@ import pt.tecnico.myDrive.exception.FileNotFoundException;
 import pt.tecnico.myDrive.exception.InvalidLoginException;
 import pt.tecnico.myDrive.exception.AccessDeniedException;
 
-
 public class DeleteFileTest extends TokenReceivingTest {
 	
 	private long rootToken;
 	private Login rootLogin;
 	MyDriveFS md ;
-	User u1 ;
-    Directory u1HomeDir;
-    
-	 
-	
+	User jade ;
+	User roxy;
+    Directory jadeHomeDir;
+
     @Override
     protected void populate() {
     	
     	md = MyDriveFS.getInstance();
-    	u1 = new User(md, "u1", "pass1", "User1", "rwxd--x-");
-    	User u2 = new User(md, "u2", "pass2", "User2", "rwxd--x-");
-    	u1HomeDir= u1.getHomeDirectory();
-    	
-		User u3 =new User(md, "u1", "pass1", "User1", "rwxd--x-");
-		Directory dirHomeDirU3 = new Directory(md, u1HomeDir, u3, "U3");
-		u3.setHomeDirectory(dirHomeDirU3);
-        
-        new TextFile(md, u1.getHomeDirectory(), u1, "tf1", "blah erase me");
-        TextFile tf2= new TextFile(md, u1HomeDir, u1, "tf2", "blah cant erase me");
-        tf2.setPermissions("rw-d----");
-        new TextFile(md, u1HomeDir, u2, "tf3", "blah erase me");
-        TextFile tf4 =new TextFile(md, u1HomeDir, u2, "tf4", "blah cant erase me");
-        tf4.setPermissions("rwxd----");
-        
-        Directory dirNotEmptyI = new Directory(md, u1HomeDir, u1, "NotEmptyI");
-        Directory dirNotEmptyII = new Directory(md, dirNotEmptyI, u1, "NotEmptyII");
-        new TextFile(md, dirNotEmptyII, u1, "tf", "blah delete me");
-        
-        Directory dirContainsCurrentDir = new Directory(md, u1HomeDir, u1, "dirContainsCurrentDir");
-        Directory dirCurrentDirU2 = new Directory(md, dirContainsCurrentDir, u1, "dccd");
-        Login u2Login= new Login(md, "u2", "pass2");
-        u2Login.setCurrentDir(dirCurrentDirU2); 
-        
-        
-        Directory dirContainsHomeDir = new Directory(md, u1HomeDir, u1, "dirContainsHomeDir");
-        Directory dirHomeDir = new Directory(md, dirContainsHomeDir, u1, "dccd"); //can homeDir be from another user??
-        u2.setHomeDirectory(dirHomeDir); 
-        
-        
-        Directory dirContainsFileWithoutPermission = new Directory(md, u1HomeDir, u1, "dcfwp");       
-        Directory dir = new Directory(md, dirContainsFileWithoutPermission, u1, "dccd");
-        TextFile tf1=new TextFile(md, dir, u2, "tf1", "blah ble black cannot delete me");
-        tf1.setPermissions("rwxd----");
-        
-        
+    	jade = new User(md, "jade", "passjade", "jade", "rwxd--x-");
+    	roxy = new User(md, "roxy", "passroxy", "roxy", "rwxd--x-");
+    	jadeHomeDir= jade.getHomeDirectory();
+    	new TextFile(md, jadeHomeDir, jade, "tf1", "blah delete me");
+		
         rootLogin = new Login(md, "root", "***");
         rootToken= rootLogin.getToken();
             
-        super.populate("u1", "pass1");
-         
+        super.populate("jade", "passjade"); 
     }
     
     
@@ -82,23 +47,28 @@ public class DeleteFileTest extends TokenReceivingTest {
     	DeleteFileService service = new DeleteFileService(validToken, "tf1");
     	service.execute();
     	
-    	assertFalse("File was not deleted", (u1HomeDir.hasFile("tf1")));	
+    	assertFalse("File was not deleted", (jadeHomeDir.hasFile("tf1")));	
     }
    
     
     @Test 
     public void sucessTextFileOfAnother(){
     	
+    	new TextFile(md, jadeHomeDir, roxy, "tf3", "blah erase me");
+    	 
     	DeleteFileService service = new DeleteFileService(validToken, "tf3");
     	service.execute();
     	
-    	assertFalse("File was not deleted", (u1HomeDir.hasFile("tf3")));
+    	assertFalse("File was not deleted", (jadeHomeDir.hasFile("tf3")));
     }
    
     
     @Test(expected=AccessDeniedException.class)
     public void failOwnTextFile(){
     	
+    	TextFile tf2= new TextFile(md, jadeHomeDir, jade, "tf2", "blah cant delete me");
+        tf2.setPermissions("rw-d----");
+        
     	DeleteFileService service = new DeleteFileService(validToken, "tf2");
     	service.execute();
     }
@@ -107,6 +77,9 @@ public class DeleteFileTest extends TokenReceivingTest {
     @Test (expected=AccessDeniedException.class)
     public void failTextFileOfAnother(){
     	
+    	TextFile tf4 =new TextFile(md, jadeHomeDir, roxy, "tf4", "blah cant delete me");
+        tf4.setPermissions("rwxd----");
+         
     	DeleteFileService service = new DeleteFileService(validToken, "tf4");
     	service.execute();
     }   
@@ -115,12 +88,12 @@ public class DeleteFileTest extends TokenReceivingTest {
     @Test 
     public void sucessRootDeleteAnyFile(){
     	
-    	rootLogin.setCurrentDir( u1.getHomeDirectory() );
-    	
-    	DeleteFileService service = new DeleteFileService(rootToken, "tf4");
+    	rootLogin.setCurrentDir(jadeHomeDir);
+        
+    	DeleteFileService service = new DeleteFileService(rootToken, "tf1");
     	service.execute();
     	
-    	assertFalse("File was not deleted", (u1HomeDir.hasFile("tf4")));
+    	assertFalse("File was not deleted", (jadeHomeDir.hasFile("tf1")));
     }
     
     
@@ -134,8 +107,11 @@ public class DeleteFileTest extends TokenReceivingTest {
     
     @Test(expected = IsHomeDirectoryException.class)
     public void failHomeDirectory(){
-    	 
-    	DeleteFileService service = new DeleteFileService(validToken, "U3");
+    	
+    	Directory newRoxyHomeDir = new Directory(md, jadeHomeDir, roxy, "newRoxyHomeDir"); 
+        roxy.setHomeDirectory(newRoxyHomeDir); 
+    	
+    	DeleteFileService service = new DeleteFileService(validToken, "newRoxyHomeDir");
     	service.execute();
     }
  
@@ -158,22 +134,38 @@ public class DeleteFileTest extends TokenReceivingTest {
     
     @Test
     public void sucessNotEmptyDirectory(){
-   
+    	
+    	Directory dirNotEmptyI = new Directory(md, jadeHomeDir, jade, "NotEmptyI");
+        Directory dirNotEmptyII = new Directory(md, dirNotEmptyI, jade, "NotEmptyII");
+        new TextFile(md, dirNotEmptyII, jade, "tf", "blah delete me");
+        
     	DeleteFileService service = new DeleteFileService(validToken, "NotEmptyI");
     	service.execute(); 	
     	
-    	assertFalse("File was not deleted", (u1HomeDir.hasFile("NotEmptyI")));
+    	assertFalse("File was not deleted", (jadeHomeDir.hasFile("NotEmptyI")));
     }
    
     
     @Test(expected= AccessDeniedException.class)
     public void failDirContainsNoPermissionFiles(){
+    	
+    	Directory dirContainsFileWithoutPermission = new Directory(md, jadeHomeDir, jade, "dirContainsFileWithoutPermission");       
+        Directory dir = new Directory(md, dirContainsFileWithoutPermission, jade, "dirAux");          
+        TextFile tf1=new TextFile(md, dir, roxy, "tf1", "blah ble black cannot delete me");
+        tf1.setPermissions("rwxd----");
+        
     	DeleteFileService service = new DeleteFileService(validToken, "dirContainsFileWithoutPermission");
     	service.execute();
     }
     
     @Test(expected= IsCurrentDirectoryException.class)
     public void failDirContainsCurrentDir(){
+    	
+    	Directory dirContainsCurrentDir = new Directory(md, jadeHomeDir, jade, "dirContainsCurrentDir");
+        Directory dirCurrentDirRoxy = new Directory(md, dirContainsCurrentDir, jade, "dirAux");      
+        Login roxyLogin= new Login(md, "roxy", "roxy");
+        roxyLogin.setCurrentDir(dirCurrentDirRoxy); 
+        
     	DeleteFileService service = new DeleteFileService(validToken, "dirContainsCurrentDir");
     	service.execute();
     }
@@ -181,6 +173,11 @@ public class DeleteFileTest extends TokenReceivingTest {
     
     @Test(expected= IsHomeDirectoryException.class)
     public void failDirContainsHomeDir(){	
+    	
+    	Directory dirContainsHomeDir = new Directory(md, jadeHomeDir, jade, "dirContainsHomeDir");
+        Directory dirHomeDir = new Directory(md, dirContainsHomeDir, roxy, "newRoxyHomeDir"); 
+        roxy.setHomeDirectory(dirHomeDir); 
+         
     	DeleteFileService service = new DeleteFileService(validToken, "dirContainsHomeDir");
     	service.execute();
     }
@@ -202,7 +199,7 @@ public class DeleteFileTest extends TokenReceivingTest {
 		ReadFileService service = new ReadFileService(validToken, "tf1");
 		service.execute();
 		
-		assertFalse("File was not deleted", (u1HomeDir.hasFile("tf1")));	
+		assertFalse("File was not deleted", (jadeHomeDir.hasFile("tf1")));	
 	}
 
 
@@ -214,7 +211,4 @@ public class DeleteFileTest extends TokenReceivingTest {
 		
 	}
     
-   
-	
-
 }
