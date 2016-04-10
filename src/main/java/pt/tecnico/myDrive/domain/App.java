9246596976baebef30e5d72a12d83e41ae919d7e
Pedro Cabral <petzichila@gmail.com>
2016-04-10 20:34:10 +0100

    Add content checking methods for Link and App

--- src/main/java/pt/tecnico/myDrive/domain/App.java
+++ src/main/java/pt/tecnico/myDrive/domain/App.java
@@ -20,6 +20,29 @@ public class App extends App_Base {
 		super();
 		xmlImport(myDrive, appElement);
 	}
+	
+    public boolean hasValidContent() {
+    	String packageParts[] = getContent().split("\\.");
+    	
+    	if (packageParts.length == 0)
+    		return false;
+    	
+    	for (String part : packageParts){
+    		char firstChar = part.charAt(0); 
+    		
+    		if (!Character.isJavaIdentifierStart(firstChar))
+    			return false;
+    		
+    		for (int i=0; i<part.length(); i++) {
+    			char nextChar = part.charAt(i);
+    			
+    			if (!Character.isJavaIdentifierPart(nextChar))
+    				return false;
+    		}
+    	}
+    	
+    	return true;
+    }
 
     @Override
      public String xmlTag() {
