c9a300e0cf5b24d8ae0e5e0960f1a4bc078819e9
Daniel Madruga <madrugadjn@gmail.com>
2016-04-11 01:59:22 +0100

    Add result() method to ListDirectoryService
    Needed for #23.

--- src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
@@ -1,8 +1,13 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.service.dto.AbstractFileDTO;
+
+import java.util.List;
+
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class ListDirectoryService extends MyDriveService {
+	List<AbstractFileDTO> result;
 
 	public ListDirectoryService(long token) {
 		// TODO 
@@ -14,5 +19,8 @@ public class ListDirectoryService extends MyDriveService {
 
 	}
 
-	
+	public final List<AbstractFileDTO> result() {
+		// TODO
+		return result; // FIXME
+	}
 }
