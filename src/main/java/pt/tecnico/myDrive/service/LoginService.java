0fafdedad3ffcd39bcd19101456ec39bb8a279e1
Daniel Madruga <madrugadjn@gmail.com>
2016-04-11 17:44:17 +0100

    Code the LoginService
    Closes #18

--- src/main/java/pt/tecnico/myDrive/service/LoginService.java
+++ src/main/java/pt/tecnico/myDrive/service/LoginService.java
@@ -1,21 +1,30 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.MyDriveFS;
+import pt.tecnico.myDrive.domain.Login;
+
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class LoginService extends MyDriveService {
-
+	
+	private String username;
+	private String password;
+	long token;
+	
 	public LoginService(String username, String password) {
-		// TODO Auto-generated constructor stub
+		this.username = username;
+		this.password = password;
 	}
 
 	@Override
 	protected void dispatch() throws MyDriveException {
-		// TODO Auto-generated method stub
-
+		MyDriveFS mD = MyDriveFS.getInstance();
+		Login login = new Login(mD, this.username, this.password);
+		token = login.getToken();
 	}
 
 	public final long result() {
-		// TODO
-		return 0; // FIXME
+		
+		return token; 
 	}
 }
