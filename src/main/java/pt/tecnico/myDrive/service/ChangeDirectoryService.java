99d3b94b9402b9a3ae6f295279493495751c986f
Catarina Cepeda <catarinacepeda@gmail.com>
2016-04-13 00:11:42 +0100

    Add Change Directory Service
    Closes #21

--- src/main/java/pt/tecnico/myDrive/service/ChangeDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/service/ChangeDirectoryService.java
@@ -1,16 +1,25 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.Directory;
+import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.MyDriveFS;
+
 public class ChangeDirectoryService extends MyDriveService {
 	
 	String path;
+	long token;
 
-	public ChangeDirectoryService(long token, String newPath) {
-		path = newPath;
+	public ChangeDirectoryService(long token, String path) {
+		this.token = token;
+		this.path = path;
 	}
 
 
 	@Override
 	public final void dispatch() {
-		
+		MyDriveFS md = getMyDrive();
+		Login login = md.getLoginByToken(token);
+		Directory newDir = md.getDirectoryByPath(login.getCurrentDir(), path);
+		login.setCurrentDir(newDir);		
 	}
 }
