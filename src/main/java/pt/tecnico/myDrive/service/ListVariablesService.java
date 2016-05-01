6e889d498a2062d71ca96d72cc2366e6d7cabb88
Catarina Cepeda <catarinacepeda@gmail.com>
2016-05-01 16:56:42 +0100

    Finish ListVariablesService
    Closes #72

--- src/main/java/pt/tecnico/myDrive/service/ListVariablesService.java
+++ src/main/java/pt/tecnico/myDrive/service/ListVariablesService.java
@@ -1,24 +1,37 @@
 package pt.tecnico.myDrive.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
+import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.Variable;
 import pt.tecnico.myDrive.exception.MyDriveException;
 import pt.tecnico.myDrive.service.dto.VariableDTO;
 
 public class ListVariablesService extends MyDriveService {
 
 	long token;
-	List<VariableDTO> result;
+	List<VariableDTO> result = new ArrayList<VariableDTO>();
 	
 	public ListVariablesService(long token) {
+		this.token = token;
 	}
 
 	@Override
 	protected void dispatch() throws MyDriveException {
-		// TODO Auto-generated method stub
+		Login login = getMyDrive().getLoginByToken(token);
+		Set<Variable> vars = login.getVariableSet();
+		
+		for (Variable v : vars) {
+			result.add(new VariableDTO(v));
+		}
+		
+		Collections.sort(result);
 	}
 	
 	public List<VariableDTO> result() {
-		return null; //TODO
+		return result;
 	}
 }
