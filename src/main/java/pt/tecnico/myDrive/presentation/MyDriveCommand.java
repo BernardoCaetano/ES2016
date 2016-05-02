c1ee2a34e143639d4c21a7f4901906639322f33f
Luís Silva <luis.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-05-02 19:45:02 +0100

    Add token saving functionality to presentation layer
    This might need some checks and exception throwing.
    Part of #77.

--- src/main/java/pt/tecnico/myDrive/presentation/MyDriveCommand.java
+++ src/main/java/pt/tecnico/myDrive/presentation/MyDriveCommand.java
@@ -2,12 +2,33 @@ package pt.tecnico.myDrive.presentation;
 
 public abstract class MyDriveCommand extends Command {
 
-	public MyDriveCommand(Shell shell, String name) {
+	public MyDriveCommand(MyDriveShell shell, String name) {
 		super(shell, name);
 	}
 
-	public MyDriveCommand(Shell shell, String name, String help) {
+	public MyDriveCommand(MyDriveShell shell, String name, String help) {
 		super(shell, name, help);
+	}
+	
+	@Override
+	protected MyDriveShell getShell() {
+		return (MyDriveShell) super.getShell();
+	}
+
+	long getToken(String username) {
+		return getShell().getToken(username);
+	}
+
+	void addToken(String username, long token) {
+		getShell().addToken(username, token);
+	}
+
+	String getCurrentUsername() {
+		return getShell().getCurrentUsername();
+	}
+
+	long getCurrentToken() {
+		return getShell().getCurrentToken();
 	}
 
 }
--- src/main/java/pt/tecnico/myDrive/presentation/MyDriveShell.java
+++ src/main/java/pt/tecnico/myDrive/presentation/MyDriveShell.java
@@ -1,6 +1,32 @@
 package pt.tecnico.myDrive.presentation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyDriveShell extends Shell {
+
+	private Map<String, Long> tokens = new HashMap<String, Long>();
+	private String currentUsername;
+	private long currentToken;
+
+	long getToken(String username) {
+		return tokens.get(username);
+	}
+
+	void addToken(String username, long token) {
+		tokens.put(username, token);
+		currentToken = token;
+		currentUsername = username;
+	}
+
+	String getCurrentUsername() {
+		return currentUsername;
+	}
+
+	long getCurrentToken() {
+		return currentToken;
+	}
+	
 
 	public MyDriveShell() {
 		super("MyDrive");
