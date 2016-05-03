477c8f687ca25189d0689bc7094a062d1e1767f3
marinaferreira9 <msf923@gmail.com>
2016-05-03 23:16:15 +0100

    Fix execute command
    Fix message on execute command
    Closes #55

--- src/main/java/pt/tecnico/myDrive/presentation/Execute.java
+++ src/main/java/pt/tecnico/myDrive/presentation/Execute.java
@@ -14,13 +14,13 @@ public class Execute extends MyDriveCommand {
 	public void execute(String[] args) {
 
 		if (args.length < 1)
-			throw new RuntimeException("USAGE: '" + getName() + " <path>' to  exexute a file in path <path> or '" + getName() + " <path> <args>' to exexute a file with the arguments <args> in path <path> ");
+			throw new RuntimeException("USAGE: '" + getName() + " <path>' to  execute a file in path <path> or '" + getName() + " <path> <args>' to execute a file with the arguments <args> in path <path> ");
 
 		if (args.length == 1) {
 			new ExecuteFileService(getCurrentToken(), args[0]).execute();
 		} else {
 			
-			ArrayList<String> al = new ArrayList<>();
+			ArrayList<String> al = new ArrayList<String>();
 
 			for (int i = 1; i < args.length; i++) {
 				al.add(args[i]);
