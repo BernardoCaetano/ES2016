26a6ea4e5ad19f850448add1e8219f42c5c7cd2d
bernardocaetano <bernas999@hotmail.com>
2016-05-06 13:53:51 +0100

    Change List Directory Service implementation
    Changed service to accept paths instead of file names
    This service tests are all good.
    Someone will need to implement new tests with path

--- src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
@@ -5,27 +5,40 @@ import pt.tecnico.myDrive.service.dto.AbstractFileDTO;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.MDC;
+
 import pt.tecnico.myDrive.domain.AbstractFile;
 import pt.tecnico.myDrive.domain.Directory;
 import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.MyDriveFS;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class ListDirectoryService extends MyDriveService {
 	long loginToken;
 	ArrayList<AbstractFileDTO> result = new ArrayList<AbstractFileDTO>();
+	String path;
 
 	public ListDirectoryService(long token) {
 		loginToken = token;
+		path = ".";
+	}
+	
+	public ListDirectoryService(long token, String path) {
+		loginToken = token;
+		this.path = path;
 	}
 
 	@Override
 	protected void dispatch() throws MyDriveException {
-		Login login = getMyDrive().getLoginByToken(loginToken);
-		Directory directory = login.getCurrentDir();
-		ArrayList<AbstractFile> files = directory.getFilesSimpleSorted(login.getUser());
+		//TODO test this service when it receives a path. expect getDirectory exceptions
+		MyDriveFS mD = getMyDrive();
+		Login login = mD.getLoginByToken(loginToken);
+		Directory currentDirectory = login.getCurrentDir();
+		Directory toListDirectory = mD.getDirectoryByPath(currentDirectory, path);
+		ArrayList<AbstractFile> files = toListDirectory.getFilesSimpleSorted(login.getUser());
 		
-		result.add(new AbstractFileDTO(directory, "."));
-		result.add(new AbstractFileDTO(directory.getParent(), ".."));
+		result.add(new AbstractFileDTO(toListDirectory, "."));
+		result.add(new AbstractFileDTO(toListDirectory.getParent(), ".."));
 		
 		for (AbstractFile file : files) {
 			result.add(new AbstractFileDTO(file));
