722410e54edc5170281d87812f948fb82b8f165e
Catarina Cepeda <catarinacepeda@gmail.com>
2016-05-08 19:48:43 +0100

    Add base for ImportMyDriveService
    References #88

---
+++ src/main/java/pt/tecnico/myDrive/service/ImportMyDriveService.java
@@ -0,0 +1,20 @@
+package pt.tecnico.myDrive.service;
+
+import org.jdom2.Document;
+
+import pt.tecnico.myDrive.exception.ImportDocumentException;
+
+public class ImportMyDriveService extends MyDriveService {
+	
+	private final Document doc; 
+	
+	public ImportMyDriveService(Document doc) {
+		this.doc = doc;
+	}
+
+	@Override
+	protected void dispatch() throws ImportDocumentException {
+		//TODO: auto-generated method stub
+	}
+
+}
