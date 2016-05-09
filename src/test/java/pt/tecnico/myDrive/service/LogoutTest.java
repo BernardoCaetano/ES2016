d0e0f47fb0642c9f32ecae2fad59a4fcb77ccd2c
marinaferreira9 <msf923@gmail.com>
2016-05-09 18:43:37 +0100

    Add Logout test

---
+++ src/test/java/pt/tecnico/myDrive/service/LogoutTest.java
@@ -0,0 +1,52 @@
+package pt.tecnico.myDrive.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.MyDriveFS;
+import pt.tecnico.myDrive.domain.User;
+import pt.tecnico.myDrive.exception.InvalidLoginException;
+
+public class LogoutTest extends TokenReceivingTest {
+
+	MyDriveFS md;
+	
+	@Override
+	protected void populate() {
+		md = MyDriveFS.getInstance();
+		new User(md, "dave", "clockwork", "dave", "rwxdrwxd", null);
+		super.populate("dave", "clockwork");
+	}
+	
+	
+	
+	@Override
+	@Test(expected = InvalidLoginException.class)
+	public void expiredSessionTest2h05minAgo() throws InvalidLoginException {
+		super.setLastActivity2h05minAgo();
+		LogoutService service = new LogoutService(validToken);
+		service.execute();
+	}
+
+	@Override
+	@Test
+	public void sessionStillValidTest1h55min() {
+		super.setLastActivity1h55minAgo();
+		LogoutService service = new LogoutService(validToken);
+		service.execute();
+
+		Login login = md.getLoginByToken(validToken);
+		assertNull("Logout failed", login);
+	}
+
+	@Override
+	@Test(expected = InvalidLoginException.class)
+	public void nonExistentTokenTest() throws InvalidLoginException {
+		LogoutService service = new LogoutService(invalidToken);
+		service.execute();
+
+	}
+
+}
