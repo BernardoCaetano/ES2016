4a306f7d92243f705044db191dbfd58362ec21c4
Luís Silva <luis.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-05-09 12:35:11 +0100

    Add base for LogoutService
    Needed for #90 and #91.

---
+++ src/test/java/pt/tecnico/myDrive/service/LogoutService.java
@@ -0,0 +1,16 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.InvalidLoginException;
+
+public class LogoutService extends MyDriveService {
+
+	public LogoutService(long token) {
+		// TODO
+	}
+
+	@Override
+	protected void dispatch() throws InvalidLoginException {
+		// TODO
+	}
+
+}
