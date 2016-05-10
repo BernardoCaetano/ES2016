b0116d44769689097162f054aca85f894b432795
Luís Silva <luis.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-05-10 17:02:16 +0100

    Add Logout service
    Closes #91.

--- src/main/java/pt/tecnico/myDrive/service/LogoutService.java
+++ src/main/java/pt/tecnico/myDrive/service/LogoutService.java
@@ -1,16 +1,22 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.MyDriveFS;
 import pt.tecnico.myDrive.exception.InvalidLoginException;
 
 public class LogoutService extends MyDriveService {
+	
+	private long token;
 
 	public LogoutService(long token) {
-		// TODO
+		this.token = token;
 	}
 
 	@Override
 	protected void dispatch() throws InvalidLoginException {
-		// TODO
+		MyDriveFS md = MyDriveFS.getInstance();
+		Login login = md.getLoginByToken(token);
+		login.cleanup();
 	}
 
 }
